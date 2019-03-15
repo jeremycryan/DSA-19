c354140f649f0486463ed0e8de02d9b6664c453b
Jeremy <jeremychristopherryan@gmail.com>
2019-03-14 23:36:56 -0400

    Found the peak

--- trees/day03/src/divide_and_conquer/PeakFinding.java
+++ trees/day03/src/divide_and_conquer/PeakFinding.java
@@ -51,13 +51,115 @@ public class PeakFinding {
 
 
     public static int findOneDPeak(int[] nums) {
-        // TODO
-        return 0;
+
+        // if list has one element, return it
+        if (nums.length == 1) return nums[0];
+
+        // Recurse!
+        return findOneDPeakRecursive(nums, 0, nums.length - 1);
+
+    }
+
+    public static int findOneDPeakRecursive(int[] nums, int start_idx, int end_idx) {
+
+
+        if (start_idx == end_idx) return start_idx;
+
+        int middle_idx = (end_idx - start_idx) / 2 + start_idx;
+        int peak_value = peakOneD(middle_idx, nums);
+
+        if (peak_value == 0) {
+            return middle_idx;
+        } else if (peak_value < 0) {
+            return findOneDPeakRecursive(nums, start_idx, middle_idx - 1);
+        } else {
+            return findOneDPeakRecursive(nums, middle_idx + 1, end_idx);
+        }
+
     }
 
     public static int[] findTwoDPeak(int[][] nums) {
-        // TODO
-        return null;
+
+        return findTwoDPeakRecursive(nums, 0, nums.length - 1, 0, nums[0].length - 1);
+
     }
+
+    public static int[] findTwoDPeakRecursive(int[][] nums, int y_min, int y_max, int x_min, int x_max) {
+
+        int ydiff = y_max - y_min;
+        int xdiff = x_max - x_min;
+        int return_vals[] = {0, 0};
+
+        if (xdiff <= ydiff) {
+
+            // Find local max in column if thing is taller than wide
+            int x_mid = (x_max - x_min)/2 + x_min;
+            int max = -999999;
+            int max_idx = 0;
+            for (int y = y_min; y <= y_max; y++) {
+                if (nums[y][x_mid] > max) {
+                    max = nums[y][x_mid];
+                    max_idx = y;
+                }
+            }
+
+            // If found value is peak, return it (base case)
+            if (peakX(x_mid, max_idx, nums) == 0) {
+                return_vals[0] = max_idx;
+                return_vals[1] = x_mid;
+                return return_vals;
+            }
+
+            // If increases in y, cut array in half lengthwise (divide)
+            else if (peakX(x_mid, max_idx, nums) > 0) {
+                // Recursive step (conquer)
+                return findTwoDPeakRecursive(nums, y_min, y_max, x_mid + 1, x_max);
+            }
+
+            // Otherwise, cut but keep the other half (divide)
+            else if (peakX(x_mid, max_idx, nums) < 0) {
+                // Recursive step (conquer)
+                return findTwoDPeakRecursive(nums, y_min, y_max, x_min, x_mid - 1);
+            }
+
+        }
+
+        if (ydiff <= xdiff) {
+
+            // Find local max in column if thing is taller than wide
+            int y_mid = (y_max - y_min)/2 + y_min;
+            int max = -999999;
+            int max_idx = 0;
+            for (int x = x_min; x <= x_max; x++) {
+                if (nums[y_mid][x] > max) {
+                    max = nums[y_mid][x];
+                    max_idx = x;
+                }
+            }
+
+            // If found value is peak, return it
+            if (peakX(max_idx, y_mid, nums) == 0) {
+                return_vals[0] = y_mid;
+                return_vals[1] = max_idx;
+                return return_vals;
+            }
+
+            // If increases in y, cut array in half lengthwise
+            else if (peakX(max_idx, y_mid, nums) > 0) {
+                return findTwoDPeakRecursive(nums, y_mid + 1, y_max, x_min, x_max);
+            }
+
+            // Otherwise, cut but keep the other half
+            else if (peakX(max_idx, y_mid, nums) < 0) {
+                return findTwoDPeakRecursive(nums, y_mid - 1, y_max, x_min, x_max);
+            }
+
+        }
+
+        return return_vals;
+
+
+    }
+
 
 }
